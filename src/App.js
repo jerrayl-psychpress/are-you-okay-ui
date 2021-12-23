39a1ed679a916cd9184e11c20ba5849aba64d911
Somethea Siek <sometheasiekswx@gmail.com>
2021-12-23 12:41:42 +1100

    Add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>
+                Dashboard
+            </h1>
+            <div>
+                <h2>
+                    Users
+                </h2>
+                <div>
+                    Somethea: Busy
+                </div>
+                <div>
+                    Kevin: Open to chat
+                </div>
+                <div>
+                    Jerrayl: Stuck on bug
+                </div>
+            </div>
+            <div>
+                <form>
+                    <input type={"text"} className={"text-red"}/>
+                    <button className={""}>
+                        Send Message
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
